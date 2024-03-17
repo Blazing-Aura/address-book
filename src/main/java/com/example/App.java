e1f51e3016947639ce98cac05ea1468a8ef1c331
Blazing-Aura <sbbarreto23@gmail.com>
2024-03-17 18:08:51 +1000

    Neccessary changes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -12,18 +12,19 @@ import java.io.IOException;
  * JavaFX App
  */
 public class App extends Application {
+    // Constants defining the window title and size
+    public static final String TITLE = "Address Book";
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 360;
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("hello-view"), WIDTH, HEIGHT);
+        stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
